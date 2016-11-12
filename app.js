db2b1f73295fab3daf8528852d79ba80ea756c0e
kaminduN <kaminduN@users.noreply.github.com>
2016-11-12 18:10:49 +0530

    adding basic conversation capabiliy to bot

--- app.js
+++ app.js
@@ -10,14 +10,14 @@ var app = express();
 
 // Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "",
-    appPassword: ""
+    appId: process.env.MICROSOFT_APP_ID || "",
+    appPassword: process.env.MICROSOFT_APP_PASSWORD || ""
 });
 var bot = new builder.UniversalBot(connector);
 
 app.post('/api/messages', connector.listen());
 
-var listner = app.listen(process.env.port || process.env.PORT || 3978, function () {
+var listner = app.listen(process.env.port || process.env.PORT || 3000, function () {
    console.log('%s listening to %s', app.name, listner.address().port); 
 });
 
@@ -25,7 +25,16 @@ var listner = app.listen(process.env.port || process.env.PORT || 3978, function 
 //=========================================================
 // Bots Dialogs
 //=========================================================
+String.prototype.contains = function(content){
+  return this.indexOf(content) !== -1;
+}
 
 bot.dialog('/', function (session) {
-    session.send("Hello World");
+    if(session.message.text.toLowerCase().contains('hello')){
+			session.send(`Hey, How are you?`);
+      }else if(session.message.text.toLowerCase().contains('help')){
+			session.send('How can I help you?');
+      }else{
+			session.send('Sorry I don\'t understand you...');
+      }
 });
