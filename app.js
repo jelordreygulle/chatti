7b4ee9251245da5ffe2d7e0fc9e650843eadcf0d
Kamindu <kaminduN@users.noreply.github.com>
2017-07-12 00:32:48 +0530

    Adding more functionality to bot

--- app.js
+++ app.js
@@ -36,18 +36,63 @@ String.prototype.contains = function(content){
 bot.dialog('/', function (session) {
     console.log(session.message.user.name + ' -> ' +session.message.text.toLowerCase());
     if(session.message.text.toLowerCase().contains('hello') || session.message.text.toLowerCase().contains('hi')){
-			session.send('Hey, '+ session.message.user.name +' How are you?');
-      //}else if(session.message.text.toLowerCase().contains('help')){
-	//		session.send('How can I help you?');
-      }else if(session.message.text.toLowerCase().contains('good morning')){
-			session.send('Good morning');
+			          // console.log('user '+ session.userData.name)
+        if (session.userData.name === undefined){
+          session.userData.name = session.message.user.name
+        }
+
+        session.send('Hey, '+ session.userData.name +' How are you?');
+
+      }else if((m = /^\good (morning|evening)/i.exec(session.message.text)) !== null){
+        greets = Array('Good day to you too', 'Wish you the same', 'hmm');
+        session.send(greets[Math.floor(Math.random()*greets.length)]);
+      }else if(/^change name/i.test(session.message.text)){
+          session.beginDialog('/changeName');
+       
       }else if(session.message.text.toLowerCase().contains('thank')){
-			session.send('You are welcome');
+			  session.send('You are welcome');
       }else{
-			session.send('Sorry I don\'t understand you...');
+			  session.send('Sorry I don\'t understand you...');
       }
 });
 
-bot.dialog('HelpDialog', function (session) {
-       session.send('How can I help you?').endDialog();
-}).triggerAction({ matches: [/help/i, /support/i, /problem/i] });
+bot.dialog('/changeName', [
+    function (session, next) {
+        session.dialogData.NewName = " ";
+        builder.Prompts.text(session, "Well that's new. What do you want me to call you?");
+    },
+    function (session, results) {
+        session.dialogData.NewName = results.response;
+        builder.Prompts.choice(session, "Are you sure you want me to call you " + session.dialogData.NewName + "?", ["Yes", "No"]);
+    },
+    function (session, results) {
+        if (results.response.entity == "Yes") {
+            session.userData.name = session.dialogData.NewName;
+            session.endDialog("Alright " + session.userData.name + "!!!");
+        }
+        else {
+            session.endDialog("Phew! I am anyways not good at remembering names.");
+        }
+    }
+]);
+
+// search dialog
+bot.dialog('search', function (session, args, next) {
+    // perform search
+    // console.log(args.intent.matched[1].trim());
+    var messageText = args.intent.matched[1].trim();
+    // session.send('%s, wait a few seconds. Searching for \'%s\' ...', session.message.user.name, messageText);
+    session.send('https://www.bing.com/search?q=%s', encodeURIComponent(messageText));
+    session.endDialog();
+}).triggerAction({ matches: /^search (.*)/i });//regex pattern matching
+
+bot.dialog('HelpDialog', require('./support'))
+    .triggerAction({ 
+      matches: [/help/i, /support/i, /problem/i] 
+});
+
+// reset bot dialog
+bot.dialog('reset', function (session) {
+    delete session.userData.name
+    session.endDialog('Ups... I\'m suffering from a memory loss...');
+}).triggerAction({ matches: /^reset/i });
