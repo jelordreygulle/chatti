179be5eda8425f228a2396a1c4f320a32d4446b4
Kamindu <kaminduN@users.noreply.github.com>
2017-07-14 22:54:32 +0530

    Update support.js

--- support.js
+++ support.js
@@ -1,3 +1,12 @@
 module.exports = function (session) {
-    session.send('How can I help you?').endDialog();
+    session.send('How can I help you?');
+    session.send('You know saying hi can go a long way..');
+    session.sendTyping();
+    session.send('So what can i do ?');
+    session.send('I can search things');
+    session.send('I can have a small chat');
+    session.sendTyping();
+    session.sendTyping();
+    session.send('Im still learning so.. don\'t be angry if i made a mistake');
+    session.endDialog();
 };
